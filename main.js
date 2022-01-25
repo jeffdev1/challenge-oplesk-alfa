4d70f03080fec0ad3dca2ecaf02701b7d2b261ce
jeffdev1 <soyjeffersontua@gmail.com>
2022-01-24 23:37:33 -0400

    Se termino el programa de verificacion y se esta organizando el codigo

--- main.js
+++ main.js
@@ -19,35 +19,48 @@ const datosPersona = {
     edad: '',
 }
 
+const separador = '#'.repeat(100);
 
 // -------------EJECUCION DEL SOFTWARE-------------
-// Aceptacion de terminos y condiciones del programa a ejecutarse proximamente
+// Aceptacion de terminos y condiciones del programa a ejecutarse proximamente (True[y] / False[n])
 if(readlineSync.keyInYN('Entiende usted que la finalidad de este verificador es ayudar a las autoridades del aeropuesto a tomar decisiones y asi evitar la propagacion del COVID-19?'.green)) {
-    // Condicional para tomar los datos del usuario que ejecuta el programa o los de la persona a la que representa
-    if(readlineSync.keyInYN(`${'#'.repeat(100)}\nEsta respondiendo por usted mismo?`.green)) {
+
+    // Condicional para tomar los datos del usuario o los de la persona a la que representa (True[y] / False[n])
+    if(readlineSync.keyInYN(`${separador}\nEsta respondiendo por usted mismo?`.green)) {
+
         // Preguntas para tomar los datos personales
         datosPersona.nombreApellido = readlineSync.question('Cual es su nombre y apellido?  '.green);
-        datosPersona.nacionalidad = readlineSync.question(`${'#'.repeat(100)}\nHola ${datosPersona.nombreApellido}, cual es tu nacionalidad?  `.green);
-        datosPersona.edad = readlineSync.question(`${'#'.repeat(100)}\n${datosPersona.nombreApellido} que edad tienes?  `.green);
+        datosPersona.nacionalidad = readlineSync.question(`${separador}\nHola ${datosPersona.nombreApellido}, cual es tu nacionalidad?  `.green);
+        datosPersona.edad = readlineSync.question(`${separador}\n${datosPersona.nombreApellido} que edad tienes?  `.green);
+        
+        // Condicional de la vacuna 1ra etapa del sistema (Condicional descartativa) (True[y] / False[n])
+        if(readlineSync.keyInYN(`${separador}\n¿Está completamente vacunado contra el COVID-19? se considera completamente vacunado 2 semanas después de ponerse la segunda dosis de una serie de dos dosis, como en el caso de las vacunas Pfizer-BioNTech o Moderna; o 2 semanas después de ponerse una vacuna de una sola dosis, como en el caso de la vacuna Janssen de Johnson & Johnson.`.green)) {
 
-        // Condicional de la vacuna para entrar a la 2da fase del sistema (Condicional descartativa)
-        if(readlineSync.keyInYN(`${'#'.repeat(100)}\n¿Está completamente vacunado contra el COVID-19? se considera completamente vacunado 2 semanas después de ponerse la segunda dosis de una serie de dos dosis, como en el caso de las vacunas Pfizer-BioNTech o Moderna; o 2 semanas después de ponerse una vacuna de una sola dosis, como en el caso de la vacuna Janssen de Johnson & Johnson.`.green)) {
             // Etapa 2 del software de autoverificacion de covid-19 (Etapa descartativa)
-            if(readlineSync.keyInYN(`${'#'.repeat(100)}\n¿Tiene alguno de estos síntomas potencialmente mortales? Dificultad para respirar, dolor o presión persistentes en el pecho, Estado de confusión de aparición reciente, No puede despertarse o permanecer despierto y/o Color pálido, gris o azulado de la piel, los labios, o el lecho de las uñas, dependiendo del tono de piel`.green)) {
-                console.log(`${'#'.repeat(100)}\nSegún sus síntomas, es posible que necesite atención médica de urgencia. vaya a la sala de emergencias del aeropuerto.`.red);
+            if(readlineSync.keyInYN(`${separador}\n¿Tiene alguno de estos síntomas potencialmente mortales? Dificultad para respirar, dolor o presión persistentes en el pecho, Estado de confusión de aparición reciente, No puede despertarse o permanecer despierto y/o Color pálido, gris o azulado de la piel, los labios, o el lecho de las uñas, dependiendo del tono de piel`.green)) {
+
+                // Descarte por sintomas de covid-19
+                console.log(`${separador}\nSegún sus síntomas, es posible que necesite atención médica de urgencia. vaya a la sala de emergencias del aeropuerto.`.red);
+
+                // Inyeccion de la razon al objeto de los datos del usuario
                 datosPersona.razonDelRechazo = 'Necesita atencion medica';
+                
             // Etapa 3 del software de autoverificacion de covid-19 (Etapa Descartativa)
             } else {
                 if(readlineSync.keyInYN(`${'#'.repeat(100)}\nEn las últimas dos semanas, ¿tuvo contacto cercano con alguien que tuviera COVID-19? Excluya a las personas que habían tenido COVID-19 en los 3 meses anteriores.`.green)) {
-                    console.log(`El pasajero no puede abordar`);
+                    console.log(`El sistema determino que usted representa un riesgo para la salud publica de las naciones, por lo que no puede abordar a el vuelo correspondiente, le pedimos que se dirija de manera inmediata a la sala de emergencias del aeropuerto`.red);
+                    datosPersona.razonDelRechazo = 'Contanto directo con una persona covid-19 positivo';
                 } else {
-                    console.log(`El pasajero puede abordar el avion`);
+                    console.log(`El sistema determino que usted no representa un riesgo para la salud publica de las naciones, por lo que puede abordar a el vuelo correspondiente, le deseamos un feliz viaje`.blue);
                 }
             }
+
+        // Descarte de abordaje por no cumplir con los requisitos de vacunacion
         } else {
-            console.log(`${'#'.repeat(100)}\nUsted no es apto para abordar su vuelo ya que no cumple con los requisitos de vacunacion antes mencionados`.red);
+            console.log(`${separador}\nUsted no es apto para abordar su vuelo ya que no cumple con los requisitos de vacunacion antes mencionados`.red);
             datosPersona.razonDelRechazo = 'No esta vacunado';
         }
+
     // Toma de datos de la persona a la que representa
     } else {
         // Preguntas para tomar los datos del 3ro al que representa
@@ -62,13 +75,19 @@ if(readlineSync.keyInYN('Entiende usted que la finalidad de este verificador es 
                 datosPersona.razonDelRechazo = 'Necesita atencion medica';
             // Etapa 3 del software de autoverificacion de covid-19 (Etapa Descartativa)
             } else {
-                console.log("entrando en etapa 3".green);
+                if(readlineSync.keyInYN(`${'#'.repeat(100)}\nEn las últimas dos semanas, ¿tuvo contacto cercano con alguien que tuviera COVID-19? Excluya a las personas que habían tenido COVID-19 en los 3 meses anteriores.`.green)) {
+                    console.log(`El sistema determino que usted representa un riesgo para la salud publica de las naciones, por lo que no puede abordar a el vuelo correspondiente, le pedimos que se dirija de manera inmediata a la sala de emergencias del aeropuerto`.red);
+                    datosPersona.razonDelRechazo = 'Contanto directo con una persona covid-19 positivo';
+                } else {
+                    console.log(`El sistema determino que usted no representa un riesgo para la salud publica de las naciones, por lo que puede abordar a el vuelo correspondiente, le deseamos un feliz viaje`.blue);
+                }
             }
         } else {
             console.log(`${'#'.repeat(100)}\nUsted no es apto para abordar su vuelo ya que no cumple con los requisitos de vacunacion antes mencionados`.red);
             datosPersona.razonDelRechazo = 'No esta vacunado';
         }
     }
+
 // Mensaje mostrado si los terminos y condiciones iniciales no son aceptados
 } else {
     console.log(`${'#'.repeat(100)}\nEs obligatorio que usted realice este verificador de manera satisfactoria para poder seguir al siguiente proceso del abordaje a su vuelo`)
